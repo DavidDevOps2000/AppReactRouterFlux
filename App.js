1c82b672323df79ff1af7cea46e53a3e7df5999f
DavidDevops2000 <DavidDevops2000@gmail.com>
2020-10-29 22:37:40 -0300

    adicionanando imgs

--- App.js
+++ App.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Principal from './src/components/Principal';
+import {Text, View} from 'react-native';
 
 const App: () => React$Node = () => {
-  return (
-    <View>
-      <Text>oi</Text>
-    </View>
-  );
+  return (<Principal/>)
 };
 
 export default App;
